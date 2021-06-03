002731b1dc2b272d98ff6d76df5036101cf3050e
Shon Harsh <shon.harsh@gmail.com>
2021-06-03 11:17:57 -0400

    Add default note examples

---
+++ src/DefaultNoteFile.js
@@ -0,0 +1,105 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Question: How many programmers does it take to change a light bulb? Answer: None – It is a hardware problem."
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content: "Question: Why did the programmer quit his job? Answer: Because he did not get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content: "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Big ideas",
+    content: "Stay hydrated, rested and diligant"
+  },
+  {
+    key: 6,
+    title: "World Of Warcraft Raid Notes",
+    content: "QUACK! QUACK! QUACK! QUACK! QUAAAACK! QUACK! QUACK! QUACK! QUACK!"
+  },
+  {
+    key: 7,
+    title: "Presupposition",
+    content: "Science is inherently flawed because it does not establish God correctly"
+  },
+  {
+    key: 8,
+    title: "Psalm 119:1–8 [ESV]",
+    content: "Aleph\n\n[1] Blessed are those whose way is blameless,\nwho walk in the law of the LORD!\n[2] Blessed are those who keep his testimonies,\nwho seek him with their whole heart,\n[3] who also do no wrong,\nbut walk in his ways!\n[4] You have commanded your precepts\nto be kept diligently.\n[5] Oh that my ways may be steadfast\nin keeping your statutes!\n[6] Then I shall not be put to shame,\nhaving my eyes fixed on all your commandments.\n[7] I will praise you with an upright heart,\nwhen I learn your righteous rules.\n[8] I will keep your statutes;\ndo not utterly forsake me!"
+  },
+  {
+    key: 9,
+    title: "Doxology",
+    content: "Praise God from whom all blessings flow\nPraise Him all creatures here below\nPraise Him above ye heavenly hosts\nPraise Father Son and Holy Ghost"
+  },
+  {
+    key: 10,
+    title: "Alistair Begg",
+    content: "The main things are the plain things,\nand the plain things are the main things."
+  },
+  {
+    key: 11,
+    title: "C. S. Lewis",
+    content: "I believe in Christianity as I believe that the sun has risen: not only because I see it, but because by it I see everything else."
+  },
+  {
+    key: 12,
+    title: "Old Anglican Prayer",
+    content: "What we know not, teach us.\nWhat we are not, make us.\nWhat we have not, give us."
+  },
+  {
+    key: 13,
+    title: "David Martyn Lloyd-Jones",
+    content: "Faith means taking the bare Word of God and acting upon it because it is the Word of God. It means believing what God says simply and solely because He has said it."
+  },
+  {
+    key: 14,
+    title: "Charles Spurgeon",
+    content: "Be content to leave your prayer in his hands, who knows when to give, and how to give, and what to give, and what to withhold."
+  },
+  {
+    key: 15,
+    title: "C.T. Studd",
+    content: "If Jesus Christ be God and died for me,\nthen no sacrifice can be too great for me to make for Him."
+  },
+  {
+    key: 16,
+    title: "Chief Superintendent Bright, Endeavour",
+    content: "A moment's courage or a lifetime of regret.\nThat's always been the choice."
+  },
+  {
+    key: 17,
+    title: "Metallica - King Nothing",
+    content: "Careful what you wish\nCareful what you say\nCareful what you wish you may regret it\nCareful what you wish you just might get it"
+  },
+  {
+    key: 18,
+    title: "Ralph Waldo Emerson",
+    content: "Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it."
+  },
+  {
+    key: 19,
+    title: "Michelle Schaffer",
+    content: "Be brave enought to suck at something new"
+  },
+  {
+    key: 20,
+    title: "Bob Carter",
+    content: "Poor planning on your part does not necessitate an emergency on mine"
+  }
+
+];
+
+export default notes;
